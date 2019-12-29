00ecd99fd0f760fcb91bdb61f0fced1f98ebd941
Sara-Alraddadi <sara.alraddadi5@outlook.com>
2019-12-30 00:12:11 +0300

    hw13

--- hw.js
+++ hw.js
@@ -8,20 +8,29 @@
 // function to return a string where every other word is in all caps.
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
 // ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
-var swapCase = function(string) {
-  // Codeeeee
-};
-​
+
+//Solution
+let words = ('hello stranger, what do we have today? ');
+const swapCase = words.split(/\s+/).map((val, index) => index % 2 ? val.toLowerCase(): val.toUpperCase()).join(' ');
+console.log(swapCase);
+
+
+
+
 // B) Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
 // see if you can use Ascii code to acomplish this
 // ex. shiftLetters('hello') // => 'ifmmp'
 // ex. (shiftLetters('abcxyz') // => "bcdyz{"
-var shiftLetters = function(string) {
-  // code!
-};
 
+//Solution
+let word = "hello";
+//let word = "abcxyz";
+let shiftLetters = word.split("").map((val, index) => {
+  return String.fromCharCode(word.charCodeAt(index) + 1);
+}).join('');
 
+console.log(shiftLetters);
 
 
 // PART 2: USE FOREACH IN THE FOLLOWING EXCERCISES
@@ -47,6 +56,18 @@ var shiftLetters = function(string) {
 // - 2 of diamonds
 // - 3 of diamonds 
 
+//Solution
+var cardsdeck = [];
+var cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+var suits = ["clubs","diamonds", "hearts", "spades"];
+
+  suits.forEach(suit => {
+    cards.forEach(card => {
+      cardsdeck.push(card + " of " + suit);
+    });
+  });
+
+  console.log(cardsdeck);
 
 // B) Word Play
 // Create a form where users may enter a sentence.
@@ -54,6 +75,17 @@ var shiftLetters = function(string) {
 // Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
 // Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
 
+//Solution
 
-
+let arr = [];
+const wordplay = (words) =>{ 
+  let array = words.split(" ")
+    array.forEach(word => {
+      if(word.length >= 3){
+        arr.push(word);
+      }
+    });
+    return console.log(arr.reverse().join(" "));
+}
+console.log(wordplay("as always hope you have an amazing day full of positivity and happiness"));
 
